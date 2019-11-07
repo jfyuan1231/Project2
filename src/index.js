0ab52a450afa301da8ff5e7721f426c10c0eff64
Junfeng <sameoneagain11@gmail.com>
2019-11-06 17:37:23 -0800

    third commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/game.css';
 import Game from './partials/Game';
+import {BOARD_WIDTH, BOARD_HEIGHT} from './settings';
 
 // create a game instance
 const game = new Game('game', 512, 256);
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,29 @@
+import {SVG_NS, PADDLE_GAP, PADDLE_HEIGHT, PADDLE_WIDTH} from "../settings.js";
+import Board from './Board';
+import Paddle from './Paddle';
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-
-		// Other code goes here...
+    this.gameElement = document.getElementById(this.element);
+    this.board = new Board(this.width, this.height);
+    this.paddle1 = new Paddle(PADDLE_WIDTH, PADDLE_HEIGHT, this.height, PADDLE_GAP, (this.height/2) - (PADDLE_HEIGHT/2));
+    this.paddle2 = new Paddle(PADDLE_WIDTH, PADDLE_HEIGHT, this.height, this.width - PADDLE_GAP - PADDLE_WIDTH, (this.height/2) - (PADDLE_HEIGHT/2));
+    // Other code goes here...
   }
 
   render() {
-		// More code goes here....
+    this.gameElement.innerHTML = '';
+
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+    // More code goes here....
+    this.board.render(svg);
+    this.paddle1.render(svg);
+    this.paddle2.render(svg);
   }
 }
